bb901ebf43af9afefe17d9609c0346646577a418
Sena <s.kilinc.ce@gmail.com>
2023-06-03 19:13:38 +0300

    added some comments and changed variables

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,7 +1,8 @@
 '''
 This project solves 8-Puzzle Game using A* Search Algorithm.
 The 8 puzzle game is a sliding puzzle that consists of a 3x3 grid with eight numbered tiles and one empty space. Empty represented as 0.
-The objective of the game is to rearrange the tiles from their initial scrambled positions to reach a specific target configuration:
+The objective of the game is to rearrange the tiles (with respect to empty tile) from their initial scrambled positions 
+to reach a specific target configuration:
 1 2 3
 4 5 6
 7 8 0
@@ -24,8 +25,8 @@ class Node:
         The g parameter represents the cost to reach the current state, 
         and the parent parameter is a reference to the parent node in the search tree. 
         If this node is the root node, then the parent parameter should be set to None.
-        Time complexity: O(1) - Constant. 
-        Space complexity: O(1) - Constant. 
+        Time complexity: O(1). 
+        Space complexity: O(1). 
         '''
         self.state = state # Current state
         self.g = g # Cost to reach current state
@@ -36,8 +37,8 @@ class Node:
         '''
         The f method returns the sum of g and h, where h is the estimated cost to reach the goal state using the heuristic function. 
         This is the cost function used by the search algorithm to determine which nodes to explore first.
-        Time complexity: O(1) - Constant. 
-        Space complexity: O(1) - Constant. 
+        Time complexity: O(1). 
+        Space complexity: O(1). 
         '''
         return self.g + self.h
 
@@ -46,8 +47,8 @@ class Node:
         The lt method is used to define the comparison operator for Node objects. With this objects can use the < operator.
         It returns True if the f value of this node is less than the f value of the other node. 
         This is used to maintain a priority queue of nodes to explore, where nodes with lower f values are explored first. 
-        Time complexity: O(1) - Constant. 
-        Space complexity: O(1) - Constant. 
+        Time complexity: O(1). 
+        Space complexity: O(1). 
         '''
         return self.f() < other.f()
 
@@ -55,8 +56,8 @@ def heuristicManhattan(state):
     '''
     This function computes the Manhattan distance heuristicManhattan for the given state.
     It takes a 2D list representing the puzzle state as input and returns an integer representing the heuristic cost.
-    Time complexity: O(n^2) Quadratic, where n is the size of the puzzle (n=3).
-    Space complexity: O(1) Constant, since the function uses a constant amount of memory.
+    Time complexity: O(n^2), where n is the size of the puzzle (n=3).
+    Space complexity: O(1), since the function uses a constant amount of memory.
     '''
     distance = 0 # This variable will be used to keep track of the Manhattan distance heuristic of the given puzzle state.
     # These nested loops iterate over each element of the 2D list state, from state[0][0] to state[2][2]
@@ -73,8 +74,8 @@ def actions(state):
     '''
     This function generates all possible actions that can be made from a given state in the 8-Puzzle Problem.
     It takes a 2D list representing the puzzle state as input and returns a list of all possible successor states.
-    Time complexity: O(n^2) Quadratic.
-    Space complexity: O(n^2) Quadratic.
+    Time complexity: O(n^2).
+    Space complexity: O(n^2).
     '''
     i = None # row
     j = None # column
@@ -103,8 +104,8 @@ def aStarSearch(start):
     This function implements the A* search algorithm.
     It takes a start node as input and returns the path from the start state to the goal state if a solution is found, otherwise returns None.
     The function uses a priority queue to keep track of the frontier and a set to keep track of the visited nodes.
-    Time complexity: O(b^d) - Exponential. In the worst case, where b is the branching factor and d is the depth of the solution, the algorithm explores all possible states in the search space.
-    Space complexity: O(b^d) - Exponential. The algorithm keeps track of all visited states, and the priority queue can store up to b^d nodes in the worst case.
+    Time complexity: O(b^d). In the worst case, where b is the branching factor and d is the depth of the solution, the algorithm explores all possible states in the search space.
+    Space complexity: O(b^d). The algorithm keeps track of all visited states, and the priority queue can store up to b^d nodes in the worst case.
     '''
     queue = PriorityQueue()  # These lines initialize a priority queue, queue, which is used to keep track of the nodes on the frontier.
     queue.put((start.f(), start)) # The start node is added to the queue with its f-score as the priority value.
@@ -128,24 +129,24 @@ def aStarSearch(start):
                 queue.put((child.f(), child)) # the child node is added to the priority queue with its f-score as the priority value
 
 
-def solve_puzzle(start_state):
+def solvePuzzle(startState):
     '''
     This function solves the puzzle from the given start state using the A* search algorithm.
     It takes a 2D list representing the start state as input and returns the path from the start state to the goal state if a solution is found, otherwise returns None.
-    This function creates a Node object from the given start_state and passes it to a_star() to solve the puzzle.
-    Time complexity: O(b^d) - Exponential. The time complexity is the same as aStarSearch(start) since it calls that function.
-    Space complexity: O(b^d) - Exponential. The space complexity is the same as aStarSearch(start) since it calls that function.
+    This function creates a Node object from the given startState and passes it to a_star() to solve the puzzle.
+    Time complexity: O(b^d). The time complexity is the same as aStarSearch(start) since it calls that function.
+    Space complexity: O(b^d). The space complexity is the same as aStarSearch(start) since it calls that function.
     '''
-    start = Node(start_state)  # This line creates a Node object from the given start_state by calling the Node class constructor with start_state as the state parameter. The g and parent parameters are not specified, so they default to 0 and None respectively.
+    start = Node(startState)  # This line creates a Node object from the given startState by calling the Node class constructor with startState as the state parameter. The g and parent parameters are not specified, so they default to 0 and None respectively.
     path = aStarSearch(start)  # The path variable is assigned the result of this function call.
     return path # This line returns the path variable, which contains either the optimal path from the start state to the goal state or None if a solution is not found.
 
 
-def print_board(state):
+def printBoard(state):
     '''
     This functions is for printing the puzzle board state.
     Time complexity: O(n^2).
-    Space complexity: O(n^2).
+    Space complexity: O(1).
     '''
     # These nested loops iterate over each element of the 2D list state
     for i in range(3):
@@ -155,19 +156,20 @@ def print_board(state):
 
 def main():
     '''
-    The main function calls the solve_puzzle function to solve the puzzle and prints the solution.
-    The time complexity of the main function is determined by the time complexity of the solve_puzzle function.(O(b^d))
-    The space complexity of the main function is determined by the space complexity of the solve_puzzle function.(O(b^d))
-
+    The main function calls the solvePuzzle function to solve the puzzle and prints the solution.
+    The time complexity of the main function is determined by the time complexity of the solvePuzzle function.
+    The space complexity of the main function is determined by the space complexity of the solvePuzzle function.
+    Time complexity: O(b^d)
+    Space complexity: O(b^d)
     '''
-    start_state = [[1, 2, 3], [0, 7, 8], [4, 5, 6]] # Set the initial state of the puzzle
-    path = solve_puzzle(start_state) # Solve the puzzle and get the solution path
-    if path is not None: # Checks if a solution was found by the solve_puzzle() function. If a solution was found (path is not None)
+    startState = [[1, 2, 3], [0, 7, 8], [4, 5, 6]] # Set the initial state of the puzzle
+    path = solvePuzzle(startState) # Solve the puzzle and get the solution path
+    if path is not None: # Checks if a solution was found by the solvePuzzle() function. If a solution was found (path is not None)
         print("Solution found!") # The code prints "Solution found!" to the console.
         print("Number of steps:",len(path)-1) # Print the number of steps in the solution path
         # Print the puzzle states in the solution path
         for state in path:
-            print_board(state) # Print the current puzzle state
+            printBoard(state) # Print the current puzzle state
             print("*********") # Print a separator line
     else: # If no solution was found
         print("No solution found.") # the code prints "No solution found."
